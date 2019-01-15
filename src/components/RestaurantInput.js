0df1c2deaf5d09d8ea61b738f2b5be196e49272e
Michael Pope <popemray1986@gmail.com>
2019-01-15 21:49:27 +0000

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { addRestaurant } from '../actions/restaurants';
 import { connect } from 'react-redux';
 
+
 export class RestaurantInput extends Component {
 
   state = {
@@ -24,6 +25,7 @@ export class RestaurantInput extends Component {
   handleOnSubmit = event => {
     event.preventDefault();
     // add missing code
+    this.props.addRestaurant(this.state)
   }
 
   render() {
@@ -49,6 +51,5 @@ export class RestaurantInput extends Component {
   }
 };
 
-
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(null, {addRestaurant})(RestaurantInput)
